7e5a623d861e44b006ca62e0f96c75dbc0b13b5f
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-07-09 22:39:42 +0300

    Js hw3 changes

--- basic JavaScript/homework3/script.js
+++ basic JavaScript/homework3/script.js
@@ -7,18 +7,7 @@ while (!num || isNaN(num) || !Number.isInteger(num)){
 }
 let num2 = null;
     num2 = +prompt("Choose your number2");
-//for (let i = 0; i <= num; i++) {
-//   if (i % 5 == 0 && i !== 0) {
-//      console.log(i);
-//   }
-//   if (num <= 4) {
-//      alert("Sorry, no numbers");
-//      break;
-//   }
-//}
-
-
-for (let i = num; i >= num2; i--) {
+for (let i = 0; i <= num; i++) {
    if (i % 5 == 0 && i !== 0) {
       console.log(i);
    }
@@ -27,3 +16,14 @@ for (let i = num; i >= num2; i--) {
       break;
    }
 }
+
+
+//for (let i = num; i >= num2; i--) {
+//   if (i % 5 == 0 && i !== 0) {
+//      console.log(i);
+//   }
+//   if (num <= 4) {
+//      alert("Sorry, no numbers");
+//      break;
+//   }
+//}
