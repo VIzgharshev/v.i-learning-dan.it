8a5531c7a620b3c3e2f9df82b544fd0ad1acc897
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-07-13 11:20:11 +0300

    hw3 correction

--- basic JavaScript/homework3/script.js
+++ basic JavaScript/homework3/script.js
@@ -5,14 +5,14 @@ while (!num || isNaN(num) || !Number.isInteger(num)){
    alert("Choose integer number!");
    num = +prompt("Choose your number", "integer plese");
 }
-let num2 = null;
-    num2 = +prompt("Choose your number2");
+
 for (let i = 0; i <= num; i++) {
-   if (i % 5 == 0 && i !== 0) {
+   if (i % 5 == 0) {
       console.log(i);
    }
    if (num <= 4) {
-      alert("Sorry, no numbers");
+      let messege = "Sorry, no numbers";
+      console.log(messege);
       break;
-   }
+   };
 }
---
+++ basic JavaScript/homework4/script.js
@@ -0,0 +1,40 @@
+'use strict'
+
+let num1 = prompt('Enter number1!');
+while (!num1 || Number.isNaN(+num1)){
+   alert('Choose number!');
+   num1 = prompt("Enter number!", num1);
+}
+num1 = +num1;
+
+let num2 = prompt('Enter number2!');
+while (!num2 || Number.isNaN(+num2)){
+   alert('Choose number!');
+   num2 = prompt("Enter number!", num2);
+}
+num2 = +num2;
+
+//while (!num2 || isNaN(num2)){
+//   alert('Choose number!');
+//   num2 = +prompt('Enter number!', num2);
+//}
+
+let operation = prompt('choose operation: +, -, /, *.');
+while (!operation || operation !== "+" || operation !== "-" || operation !== "*" || operation !== "/"){
+   alert("Choose only operator");
+   operation = prompt('choose operation: +, -, /, *.', operation);
+}
+
+if (operation == '+') {
+   let summ = (a, b) => a + b;
+   console.log(summ(num1,num2));
+} else if (operation == '-') {
+   let minus = (a, b) => a - b;
+   console.log(minus(num1,num2));
+ } else if (operation == '*') {
+   let multy = (a, b) => a * b;
+   console.log(multy(num1,num2));
+ } else if (operation == '/') {
+   let divide = (a, b) => a / b;
+   console.log(divide(num1,num2));
+ }
