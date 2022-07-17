fdf64f5b770958bb0d8c60dbd214c9844449eb0a
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-07-17 19:35:02 +0300

    correction hw4

--- basic JavaScript/homework4/script.js
+++ basic JavaScript/homework4/script.js
@@ -15,21 +15,36 @@ while (!num2 || Number.isNaN(+num2)){
 num2 = +num2;
 
 let operation = prompt('choose operation: +, -, /, *.');
-while (!operation || operation !== "+" || operation !== "-" || operation !== "*" || operation !== "/"){
-   alert("Choose only operator");
-   operation = prompt('choose operation: +, -, /, *.', operation);
+
+function calc (){
+    if (operation === '+') {
+        let summ = num1 + num2;
+        return summ;
+    } else if (operation === '-') {
+        let minus = num1 - num2;
+        return minus;
+    } else if (operation === '*') {
+        let mult = num1 * num2;
+        return mult;
+    } else if (operation === '/') {
+        let divide = num1 / num2;
+        return divide;
+    }
 }
 
-if (operation == '+') {
-   let summ = (a, b) => a + b;
-   console.log(summ(num1,num2));
-} else if (operation == '-') {
-   let minus = (a, b) => a - b;
-   console.log(minus(num1,num2));
- } else if (operation == '*') {
-   let multy = (a, b) => a * b;
-   console.log(multy(num1,num2));
- } else if (operation == '/') {
-   let divide = (a, b) => a / b;
-   console.log(divide(num1,num2));
- }
+console.log(calc())
+
+
+// if (operation == '+') {
+//     let summ = (a, b) => a + b;
+//     console.log(summ(num1,num2));
+// } else if (operation == '-') {
+//     let minus = (a, b) => a - b;
+//     console.log(minus(num1,num2));
+// } else if (operation == '*') {
+//     let multy = (a, b) => a * b;
+//     console.log(multy(num1,num2));
+// } else if (operation == '/') {
+//     let divide = (a, b) => a / b;
+//     console.log(divide(num1,num2));
+// }
