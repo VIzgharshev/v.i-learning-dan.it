577fcce4113f63255b2059bffbc63c104ebce4a1
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-08-01 16:51:24 +0300

    add homework9

--- basic JavaScript/homework9/Script.js
+++ basic JavaScript/homework9/Script.js
@@ -1,13 +1,15 @@
 let arr1 = ["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"];
 
-function getInList(a, b = Node.parentElement) {
-    b.innerHTML = '';
+function getInList(a, b = document.body) {
+    let listParent = document.createElement(b);
+    listParent.classList.add('list')
+    document.body.append(listParent);
     for (let i = 0; i < a.length; i++) {
         let li = document.createElement('li');
+        li.classList.add('list-item')
         li.innerHTML = a[i];
-        b.innerHTML += li;
+        listParent.append(li);
     }
-
 }
 
-getInList(["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"], 'document.body');
+getInList(["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"], 'ul');
