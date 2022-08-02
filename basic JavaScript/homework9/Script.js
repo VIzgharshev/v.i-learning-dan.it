0ff3613c3001b1b89c2368d5205245539da60462
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-08-02 13:45:39 +0300

    add some correction hw9

--- basic JavaScript/homework9/Script.js
+++ basic JavaScript/homework9/Script.js
@@ -1,15 +1,61 @@
 let arr1 = ["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"];
 
+// function getInList(a, b) {
+//     let listParent = document.createElement(b);
+//     listParent.classList.add('list')
+//     document.body.append(listParent);
+//     for (let i = 0; i < a.length; i++) {
+//         let li = document.createElement('li');
+//         li.classList.add('list-item')
+//         li.innerHTML = a[i];
+//         listParent.append(li);
+//     }
+// }
+//
+// if (b !== undefined) {
+//     let li = document.createElement('li');
+//     li.innerHTML = elem;
+//     let listWrapper = document.createElement(b);
+//     document.body.append(listWrapper);
+//     listWrapper.append(li);
+// } else {
+//     document.body.append(li);
+// }
+// if (b !== undefined) {
+//     let listParent = document.createElement(b);
+//     listParent.classList.add('list')
+//     document.body.append(listParent);
+//     for (let i = 0; i < a.length; i++) {
+//         let li = document.createElement('li');
+//         li.classList.add('list-item')
+//         li.innerHTML = a[i];
+//         listParent.append(li);
+//     }
+// } else {
+//     let list = a.map((elem) => {
+//         let li = document.createElement('li');
+//         li.classList.add('list-item')
+//         li.innerHTML = elem;
+//         document.body.append(li);
+//     });
+// }
+
+
 function getInList(a, b = document.body) {
-    let listParent = document.createElement(b);
-    listParent.classList.add('list')
-    document.body.append(listParent);
-    for (let i = 0; i < a.length; i++) {
-        let li = document.createElement('li');
-        li.classList.add('list-item')
-        li.innerHTML = a[i];
-        listParent.append(li);
+    const listUl = document.createElement('ul');
+    a.map((elem) => {
+        const li = document.createElement('li');
+        li.innerHTML = elem;
+        listUl.append(li);
+    });
+    if (b === document.body) {
+        document.body.append(listUl);
+    } else {
+        const ulWrapper = document.createElement(b);
+        document.body.append(ulWrapper);
+        ulWrapper.append(listUl);
     }
+
 }
 
-getInList(["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"], 'ul');
+getInList(["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"], 'div');
