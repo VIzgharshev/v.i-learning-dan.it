5a4dd8c5e9321eeb8603c1e2862c2c20ff716fae
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-08-03 20:41:30 +0300

    add optional homework15

---
+++ basic JavaScript/OTIONAL_homework1/Script.js
@@ -0,0 +1,11 @@
+let userNum = prompt('Enter your number please!');
+while (!userNum || Number.isNaN(+userNum)) {
+    userNum = +prompt('Enter your number please!');
+}
+
+// function createFactorial () {
+//     let n = userNum - 1;
+//     let result = userNum
+//     createFactorial()
+// }
+
