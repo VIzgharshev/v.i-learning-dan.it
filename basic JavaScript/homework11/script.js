be78410bb54776aad484ae52b033f22b324e87c5
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-08-10 15:09:34 +0300

    homework 11

--- basic JavaScript/homework11/script.js
+++ basic JavaScript/homework11/script.js
@@ -1,27 +1,55 @@
-let iconsOpen = document.querySelectorAll('.fa-eye');
-iconsOpen.forEach(elem => {
-    elem.addEventListener('click', event => {
-        event.target.classList.add('invisible');
-        event.target.nextElementSibling.classList.remove('invisible');
-        event.target.previousElementSibling.setAttribute('type', 'text');
-    })
-})
+const icons = document.querySelectorAll('.icon-password');
 
-let iconClose = document.querySelectorAll('.fa-eye-slash');
-let inputs = document.querySelectorAll('.input');
-
-iconClose.forEach(elem => {
+icons.forEach(elem => {
     elem.addEventListener('click', event => {
-        event.target.classList.add('invisible');
-        event.target.previousElementSibling.classList.remove('invisible');
-        for (const input of inputs) {
-            if (event.target.dataset.second) {
-                document.querySelector('.input[data-second="second"]').setAttribute('type', 'password');
-            }
-            if (event.target.dataset.first) {
-                document.querySelector('.input[data-second="first"]').setAttribute('type', 'password');
-            }
+        event.target.classList.toggle('fa-eye');
+        event.target.classList.toggle('fa-eye-slash');
+        if (event.target.classList.contains('fa-eye')) {
+            event.target.previousElementSibling.setAttribute('type', 'password');
+        } else {
+            event.target.previousElementSibling.setAttribute('type', 'text');
         }
     })
 })
 
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', event => {
+    const inputFirst = document.querySelector('.input-first').value;
+    const inputSecond = document.querySelector('.input-second').value;
+    const warning = document.querySelector('.warning-message');
+    if (inputFirst === inputSecond) {
+        warning.classList.add('hidden');
+        alert('You are welcome');
+    } else (
+        warning.classList.remove('hidden')
+    )
+})
+
+// const iconsOpen = document.querySelectorAll('.fa-eye');
+// iconsOpen.forEach(elem => {
+//     elem.addEventListener('click', event => {
+//         event.target.classList.add('invisible');
+//         event.target.nextElementSibling.classList.remove('invisible');
+//         event.target.previousElementSibling.setAttribute('type', 'text');
+//     })
+// })
+//
+// const iconClose = document.querySelectorAll('.fa-eye-slash');
+// const inputs = document.querySelectorAll('.input');
+//
+// iconClose.forEach(elem => {
+//     elem.addEventListener('click', event => {
+//         event.target.classList.add('invisible');
+//         event.target.previousElementSibling.classList.remove('invisible');
+//         for (const input of inputs) {
+//             if (event.target.dataset.first) {
+//                 document.querySelector('.first[data-second="first"]').setAttribute('type', 'password');
+//             }
+//             if (event.target.dataset.second) {
+//                 document.querySelector('.second[data-second="second"]').setAttribute('type', 'password');
+//             }
+//
+//         }
+//     })
+// })
