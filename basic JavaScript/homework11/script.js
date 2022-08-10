33205416c5fb6a908b62fb7fcc2668f0a6907eca
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-08-10 13:08:21 +0300

    homework 11

--- basic JavaScript/homework11/script.js
+++ basic JavaScript/homework11/script.js
@@ -1,11 +1,27 @@
-let icons = document.querySelectorAll('.fas');
-icons.forEach((elem) => {
-    elem.addEventListener('click', (event) => {
-        if (event.target.classList.contains('invisible') === true) {
-            event.target.classList.remove('invisible');
+let iconsOpen = document.querySelectorAll('.fa-eye');
+iconsOpen.forEach(elem => {
+    elem.addEventListener('click', event => {
+        event.target.classList.add('invisible');
+        event.target.nextElementSibling.classList.remove('invisible');
+        event.target.previousElementSibling.setAttribute('type', 'text');
+    })
+})
+
+let iconClose = document.querySelectorAll('.fa-eye-slash');
+let inputs = document.querySelectorAll('.input');
+
+iconClose.forEach(elem => {
+    elem.addEventListener('click', event => {
+        event.target.classList.add('invisible');
+        event.target.previousElementSibling.classList.remove('invisible');
+        for (const input of inputs) {
+            if (event.target.dataset.second) {
+                document.querySelector('.input[data-second="second"]').setAttribute('type', 'password');
+            }
+            if (event.target.dataset.first) {
+                document.querySelector('.input[data-second="first"]').setAttribute('type', 'password');
+            }
         }
     })
 })
 
-console.log(icons)
-// event.target.classList.toggle('invisible');
