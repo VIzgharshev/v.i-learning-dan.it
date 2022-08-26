2ee821b09f1e2a15001323c987bb2f4585123206
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-08-26 12:10:09 +0300

    corrections in hw17

--- basic JavaScript/OPTIONAL_homework17/script.js
+++ basic JavaScript/OPTIONAL_homework17/script.js
@@ -1,46 +1,56 @@
-let userName = prompt('Input your name!');
-let userLastName = prompt('Input your last name!');
+let userName = prompt("Input your name!");
+let userLastName = prompt("Input your last name!");
+
 let student = {
-    name: userName,
-    lastName: userLastName,
-}
-let checker = '';
-while (checker != null) {
-    let subject = prompt('Input subject!');
-    checker = subject;
-    if (subject === null) {
-        break
-    } else {
+  name: userName,
+  lastName: userLastName,
+  addSubject() {
+    let checker = "";
+    while (checker != null) {
+      let subject = prompt("Input subject!");
+      checker = subject;
+      if (subject === null) {
+        break;
+      } else {
         let mark = null;
-        mark = +prompt('Input your mark!');
+        mark = +prompt("Input your mark!");
         while (!mark || isNaN(mark)) {
-            let mark = +prompt('Input your mark!');
+          let mark = +prompt("Input your mark!");
         }
         student[`${subject}`] = Number(`${mark}`);
+      }
     }
-}
-let allMarks = Object.values(student);
+   },
+   checkMarks() {
+     let allMarks = Object.values(student);
+     let countMarks = 0;
+     let summAllMarks = 0;
+     let badMarks = 0;
 
-let countMarks = 0;
-let summAllMarks = 0;
-let badMarks = 0;
+     for (let i = 0; i < allMarks.length; i++) {
+       console.log(typeof allMarks[i]);
+       if (typeof allMarks[i] === "number") {
+         countMarks++;
+         summAllMarks += allMarks[i];
+         if (allMarks[i] < 4) {
+           badMarks++;
+         }
+       }
+     }
 
-for (let i = 0; i < allMarks.length; i++) {
-    console.log(typeof allMarks[i])
-    if (typeof allMarks[i] === 'number') {
-        countMarks++;
-        summAllMarks += allMarks[i];
-        if (allMarks[i] < 4) {
-            badMarks++
-        }
-    }
-}
-if (badMarks === 0) {
-    alert('Студент переведено на наступний курс!');
-}
+     if (badMarks === 0) {
+       alert(
+         `Кількість поганіх оцінок ${badMarks}! Студент переведено на наступний курс!`
+       );
+     }
 
-let middleMark = summAllMarks / countMarks;
+     let middleMark = summAllMarks / countMarks;
 
-if (7 < middleMark) {
-    alert('Студенту призначено стипендію!');
-}
+     if (7 < middleMark) {
+       alert(`Середній бал ${Math.round(middleMark)}! Студенту призначено стипендію!`);
+     }
+  }
+};
+
+student.addSubject();
+student.checkMarks();
