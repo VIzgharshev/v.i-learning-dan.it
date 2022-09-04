f7a94ba490ba3b56e2d0b8d7b85f2b88b655775e
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-09-04 18:51:13 +0300

    add gulpfile.js

---
+++ advanced html/homework2/gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+//----------Підключаємо модулі!
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync');
+
+//----------Створюю задачі для модулів
+
+
+//----------Створюю фінальні задачі gulp
+
