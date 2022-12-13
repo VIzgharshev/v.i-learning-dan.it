18fa4033c31eb5063cf2c514ba74a3465e0dbe4c
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2022-12-13 19:31:12 +0200

    some correction

--- advanced JavaScript/HOMEWORKS/homework_1/script.js
+++ advanced JavaScript/HOMEWORKS/homework_1/script.js
@@ -1,8 +1,8 @@
 class Employee {
   //start setter
   constructor(name, age, salary) {
-    this._name = name;
-    this._age = age;
+    this.name = name;
+    this.age = age;
     this._salary = salary;
   }
   //----------Name
