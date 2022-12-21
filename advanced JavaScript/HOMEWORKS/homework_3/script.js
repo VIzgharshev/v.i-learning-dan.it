45c1d1d8ccb1509924ee7f8aac9b2b2e4e62b913
Vladymyr.Izgharshev <izgharshev.vladymyr@gmail.com>
2022-12-21 14:32:53 +0200

    cor

--- advanced JavaScript/HOMEWORKS/homework_3/script.js
+++ advanced JavaScript/HOMEWORKS/homework_3/script.js
@@ -120,7 +120,6 @@
 		birth: '1975-04-05',
 	};
 
-	//const {...fullProfile} = {...satoshi2018, ...satoshi2019, ...satoshi2020}
 	const { ...fullProfile } = { ...satoshi2018, ...satoshi2019, ...satoshi2020 };
 	console.log(fullProfile);
 }
